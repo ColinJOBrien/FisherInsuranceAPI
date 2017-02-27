5bea645efd5ee84b4199f933c6db37cd743a98f5
Colin O'Brien <Colin O'Brien>
2017-02-27 18:27:37 -0500

    Still fixing

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({     
     selector: "fisher-insurance",    
-    template: './app/app.component.html'
+    templateUrl: './app.component.html'
 }) 
-     
+
 export class AppComponent { } 
