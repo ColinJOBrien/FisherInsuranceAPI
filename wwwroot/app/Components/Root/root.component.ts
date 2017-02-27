58b9a4bc8da869dfd85e906e93252b7d085064b4
Colin O'Brien <Colin O'Brien>
2017-02-27 16:27:15 -0500

    finished lab 5  #5,6,7,8

---
+++ wwwroot/app/Components/Root/root.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core'; 
+@Component({     
+    selector: 'root',     
+    templateUrl: './app/components/root/root.component.html' }) 
+
+export class RootComponent { } 
--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,7 +2,17 @@ import {Component} from "@angular/core";
 
 @Component({     
     selector: "fisher-insurance",    
-    template: `<home-page>` 
+    template: `<home-page>`
+}) 
+
+@Component({     
+    selector: "fisher-insurance",    
+    template: `<root>`
+}) 
+
+@Component({     
+    selector: "fisher-insurance",    
+    template: `<nav-bar>`
 }) 
      
 export class AppComponent { } 
