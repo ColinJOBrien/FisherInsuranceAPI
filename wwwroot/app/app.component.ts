58b2247fa0d12905b1e039a3ddb4f1847e80f88a
Colin O'Brien <Colin O'Brien>
2017-02-27 17:26:36 -0500

    messing around

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,17 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({     
     selector: "fisher-insurance",    
-    template: `<home-page>`
-}) 
-
-@Component({     
-    selector: "fisher-insurance",    
-    template: './app.component.html'
-}) 
-
-@Component({     
-    selector: "fisher-insurance",    
-    template: `<nav-bar>`
+    template: './app/app.component.html'
 }) 
      
 export class AppComponent { } 
